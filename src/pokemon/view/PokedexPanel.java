9d74d8353a55af0cb3f008e3a0f31dc46a176d63
Bryan Tracy <btracy337@gmail.com>
2019-02-12 09:03:26 -0700

    Made buildPokedexText and updatePokemon methods.

--- src/pokemon/view/PokedexPanel.java
+++ src/pokemon/view/PokedexPanel.java
@@ -12,6 +12,8 @@ public class PokedexPanel extends JPanel
 {
 private PokedexController app;
 private SpringLayout appLayout;
+private JButton changeButton;
+private JComboBox pokedexDropdown;
 private JTextField healthPointsText;
 private JTextField attackPointsText;
 private JTextField enhancementModifierText;
@@ -24,15 +26,37 @@ private JLabel enhancementModifierLabel;
 private JLabel numberLabel;
 private JLabel nameLabel;
 private JLabel canEvolveLabel;
+private JLabel imageLabel;
 
 public PokedexPanel(PokedexController app)
 {
+	super();
+	this.app = app;
+	healthPointsText = new JTextField("0");
+	attackPointsText = new JTextField("0");
+	enhancementModifierText = new JTextField("0");
+	numberText = new JTextField("0");
+	nameText = new JTextField("Pokemon");
+	canEvolveText = new JTextField("false");
 	
+	healthPointsLabel = new JLabel("This Pokemon's health is");
+	attackPointsLabel = new JLabel("This Pokemon's attack level is");
+	enhancementModifierLabel = new JLabel("This Pokemon's ehancement level is");
+	numberLabel = new JLabel("This Pokemon's number is");
+	nameLabel = new JLabel("My name is");
+	canEvolveLabel = new JLabel("This Pokemon can evolve");
+	imageLabel = new JLabel("Your Pokemon here");
+	
+	changeButton = new JButton("Click here to change the pokevalues");
+	pokedexDropdown = new JComboBox(); //stub
+	
+	setupPanel();
+	setupLayout();
+	setupListeners();
 }
 
 private void setupPanel()
 {
-	this.setLayout(appLayout);
 	this.add(healthPointsText);
 	this.add(attackPointsText);
 	this.add(enhancementModifierText);
@@ -45,6 +69,10 @@ private void setupPanel()
 	this.add(numberLabel);
 	this.add(nameLabel);
 	this.add(canEvolveLabel);
+	this.add(changeButton);
+	this.add(imageLabel);
+	this.add(pokedexDropdown);
+	this.setLayout(appLayout);
 }
 private void setupLayout()
 {
