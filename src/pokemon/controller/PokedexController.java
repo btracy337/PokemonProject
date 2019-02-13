992850525d71da7078b657d21d4242ff9625d16d
Bryan Tracy <btracy337@gmail.com>
2019-02-13 07:38:17 -0700

    Added methods to controller and panel

--- src/pokemon/controller/PokedexController.java
+++ src/pokemon/controller/PokedexController.java
@@ -1,9 +1,42 @@
 package pokemon.controller;
+
 import java.util.ArrayList;
+
+import pokemon.model.Pokemon;
+
 public class PokedexController
 {
-public void start()
-{
-	
-}
+	public ArrayList<Pokemon> pokemonList;
+	public void start()
+	{
+		this.pokemonList = new ArrayList<Pokemon>();
+	}
+
+	public PokedexController()
+	{
+
+	}
+
+	public String[] buildPokedexText()
+	{
+		String[] names = new String[pokemonList.size()];
+		for (int index = 0; index < pokemonList.size(); index++)
+		{
+			names[index] = pokemonList.get(index).getName();
+		}
+		return names;
+	}
+	public void updatePokemon(int index, String [] data)
+	{
+		if(data.length == 5)
+		{
+			Pokemon current = pokemonList.get(index);
+			current.setAttackPoints(Integer.parseInt(data[0]));
+			current.setEnhancementModifier(Double.parseDouble(data[1]));
+			current.setHealthPoints(Integer.parseInt(data[2]));
+			current.setName(data[3]);
+			current.setCanEvolve(Boolean.parseBoolean(data[4]));
+		}
+	}
+
 }
--- src/pokemon/view/PokedexPanel.java
+++ src/pokemon/view/PokedexPanel.java
@@ -1,87 +1,89 @@
 package pokemon.view;
+
 import pokemon.controller.PokedexController;
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-
-import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class PokedexPanel extends JPanel
 {
-private PokedexController app;
-private SpringLayout appLayout;
-private JButton changeButton;
-private JComboBox pokedexDropdown;
-private JTextField healthPointsText;
-private JTextField attackPointsText;
-private JTextField enhancementModifierText;
-private JTextField numberText;
-private JTextField nameText;
-private JTextField canEvolveText;
-private JLabel healthPointsLabel;
-private JLabel attackPointsLabel;
-private JLabel enhancementModifierLabel;
-private JLabel numberLabel;
-private JLabel nameLabel;
-private JLabel canEvolveLabel;
-private JLabel imageLabel;
+	private PokedexController app;
+	private SpringLayout appLayout;
+	private JButton changeButton;
+	private JComboBox<String> pokedexDropdown;
+	private JTextField healthPointsText;
+	private JTextField attackPointsText;
+	private JTextField enhancementModifierText;
+	private JTextField numberText;
+	private JTextField nameText;
+	private JTextField canEvolveText;
+	private JLabel healthPointsLabel;
+	private JLabel attackPointsLabel;
+	private JLabel enhancementModifierLabel;
+	private JLabel numberLabel;
+	private JLabel nameLabel;
+	private JLabel canEvolveLabel;
+	private JLabel imageLabel;
 
-public PokedexPanel(PokedexController app)
-{
-	super();
-	this.app = app;
-	healthPointsText = new JTextField("0");
-	attackPointsText = new JTextField("0");
-	enhancementModifierText = new JTextField("0");
-	numberText = new JTextField("0");
-	nameText = new JTextField("Pokemon");
-	canEvolveText = new JTextField("false");
-	
-	healthPointsLabel = new JLabel("This Pokemon's health is");
-	attackPointsLabel = new JLabel("This Pokemon's attack level is");
-	enhancementModifierLabel = new JLabel("This Pokemon's ehancement level is");
-	numberLabel = new JLabel("This Pokemon's number is");
-	nameLabel = new JLabel("My name is");
-	canEvolveLabel = new JLabel("This Pokemon can evolve");
-	imageLabel = new JLabel("Your Pokemon here");
-	
-	changeButton = new JButton("Click here to change the pokevalues");
-	pokedexDropdown = new JComboBox(); //stub
-	
-	setupPanel();
-	setupLayout();
-	setupListeners();
-}
+	public PokedexPanel(PokedexController app)
+	{
+		super();
+		this.app = app;
+		healthPointsText = new JTextField("0");
+		attackPointsText = new JTextField("0");
+		enhancementModifierText = new JTextField("0");
+		numberText = new JTextField("0");
+		nameText = new JTextField("Pokemon");
+		canEvolveText = new JTextField("false");
+		healthPointsLabel = new JLabel("This Pokemon's health is");
+		attackPointsLabel = new JLabel("This Pokemon's attack level is");
+		enhancementModifierLabel = new JLabel("This Pokemon's ehancement level is");
+		numberLabel = new JLabel("This Pokemon's number is");
+		nameLabel = new JLabel("My name is");
+		canEvolveLabel = new JLabel("This Pokemon can evolve");
+		imageLabel = new JLabel("Your Pokemon here");
+		changeButton = new JButton("Click here to change the pokevalues");
+		pokedexDropdown = new JComboBox<String>(); // stub
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
 
-private void setupPanel()
-{
-	this.add(healthPointsText);
-	this.add(attackPointsText);
-	this.add(enhancementModifierText);
-	this.add(numberText);
-	this.add(nameText);
-	this.add(canEvolveText);
-	this.add(healthPointsLabel);
-	this.add(attackPointsLabel);
-	this.add(enhancementModifierLabel);
-	this.add(numberLabel);
-	this.add(nameLabel);
-	this.add(canEvolveLabel);
-	this.add(changeButton);
-	this.add(imageLabel);
-	this.add(pokedexDropdown);
-	this.setLayout(appLayout);
-}
-private void setupLayout()
-{
-	
-	
-}
-private void setupListeners()
-{
-	
-	
-}
+	private void setupPanel()
+	{
+		this.add(healthPointsText);
+		this.add(attackPointsText);
+		this.add(enhancementModifierText);
+		this.add(numberText);
+		this.add(nameText);
+		this.add(canEvolveText);
+		this.add(healthPointsLabel);
+		this.add(attackPointsLabel);
+		this.add(enhancementModifierLabel);
+		this.add(numberLabel);
+		this.add(nameLabel);
+		this.add(canEvolveLabel);
+		this.add(changeButton);
+		this.add(imageLabel);
+		this.add(pokedexDropdown);
+		this.setLayout(appLayout);
+	}
+
+	private void setupLayout()
+	{
+		
+	}
+
+	private void setupListeners()
+	{
+
+	}
+	private void setupDropdown() {
+		DefaultComboBoxModel<String> temp = new DefaultComboBoxModel<String>(app.buildPokedexText);
+		pokedexDropdown.setModel(temp);
+		
+	}
+
 }
